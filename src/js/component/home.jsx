323d456d93a064fce588330490fe713ba90ec037
Carlos beltran <137569880+DodLitros@users.noreply.github.com>
2023-10-17 16:23:56 +0000

    first comit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+	const[counter, setCounter] = useState(0)
+	const[stop, setStop] = useState(true)
+
+
+	useEffect(()=>{
+		if(stop){
+
+			let interval = setInterval(() => {
+				setCounter(counter+1)
+			}, 1000 );
+			
+			return ()=> clearInterval(interval); 
+			}
+
+		}
+		
+		,[counter, stop])
+
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="contenedor">
+			<h1>Contador</h1>
+			<div className="contador">
+				<h1>{ Math.floor((counter/10000)%10) }</h1>
+				<h1>{ Math.floor((counter/1000)%10) }</h1>
+				<h1>{ Math.floor((counter/100)%10) }</h1>
+				<h1>{ Math.floor((counter/10)%10) }</h1>
+				<h1>{ Math.floor((counter/1)%10) }</h1>
+			</div>
+			<div className="botones">
+				<button onClick= { ()=>setStop(!stop) }> {stop?'Stop':'Resume'}</button>
+				<button onClick= { ()=>setCounter(counter==0) }> Restart</button>
+
+			</div>
 		</div>
 	);
 };
